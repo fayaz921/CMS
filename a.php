765cea0bca72d7b509e5d560e28d5f84e03aeb8a
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-11 20:31:39 +0500

    Update a.php

--- a.php
+++ a.php
@@ -5,6 +5,7 @@ session_start(); // Start the session to manage user login state
 <!doctype html>
 <html lang="en">
 <!--begin::Head-->
+
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title> Login</title>
