04d7542268b0f8645cc7891621664e7428df767d
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-15 21:07:24 +0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@
 define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'cms_db');
+define('DB_NAME', 'cms');
 
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if ($mysqli->connect_error) {
