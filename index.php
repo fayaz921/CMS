73af238107b86a142546b2637006819cff83f3c3
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-15 20:45:23 +0500

    Update index.php

--- index.php
+++ index.php
@@ -8,17 +8,17 @@
 </head>
 <body>
     <?php
-    include_once "./views/layout/header.php";
-    ?>
-       <!--begin::App Main-->
+// main page - include header (head + opening body + sidebar), then page content, then footer (scripts + closing tags)
+include_once __DIR__ . "/views/layout/header.php";
+?>
+
+      <!--begin::App Main-->
       <main class="app-main">
         <!--begin::App Content Header-->
         <div class="app-content-header">
-          <!--begin::Container-->
           <div class="container-fluid">
-            <!--begin::Row-->
             <div class="row">
-              <div  class="col-sm-6"><h3 class="mb-0">Dashboard</h3></div>
+              <div class="col-sm-6"><h3 class="mb-0">Dashboard</h3></div>
               <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-end">
                   <li class="breadcrumb-item"><a href="#">Home</a></li>
@@ -26,14 +26,11 @@
                 </ol>
               </div>
             </div>
-            <!--end::Row-->
           </div>
-          <!--end::Container-->
         </div>
-        <!--end::App Content Header-->
+
         <!--begin::App Content-->
         <div class="app-content">
-          <!--begin::Container-->
           <div class="container-fluid">
             <!--begin::Row-->
             <div class="row">
@@ -454,8 +451,8 @@
       </main>
       <!--end::App Main-->
 
-       <?php
-    include_once "./views/layout/footer.php";
-    ?>
+<?php
+include_once __DIR__ . "/views/layout/footer.php";
+?>
 </body>
 </html>
