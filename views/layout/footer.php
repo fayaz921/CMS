1ee87ed8e59cdce8934a58f49a5a8580f2a6ef29
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-15 20:37:13 +0500

    Update footer.php

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -22,8 +22,6 @@
         <!--end::Copyright-->
     </footer>
     <!--end::Footer-->
-    </div>
-
 
      <!--end::App Wrapper-->
     <!--begin::Script-->
@@ -43,7 +41,7 @@
       crossorigin="anonymous"
     ></script>
     <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
-    <script src="./js/adminlte.js"></script>
+    <script src="/CMS/assets/js/adminlte.js"></script>
     <!--end::Required Plugin(AdminLTE)--><!--begin::OverlayScrollbars Configure-->
     <script>
       const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
